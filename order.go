619502396b9974791c500b2cc5dff7f481c85430
MinhLoc282 <minhloc2802@gmail.com>
2023-10-31 22:15:01 +0700

    add userId to order

--- order.go
+++ order.go
@@ -2,10 +2,11 @@ package hftorderbook
 
 // Single Order in an order book, as a node in a LimitOrder FIFO queue
 type Order struct {
-	Id int
-	Volume float64
-	Next *Order
-	Prev *Order
-	Limit *LimitOrder
+	Id       int
+	UserId   string
+	Volume   float64
+	Next     *Order
+	Prev     *Order
+	Limit    *LimitOrder
 	BidOrAsk bool
 }
